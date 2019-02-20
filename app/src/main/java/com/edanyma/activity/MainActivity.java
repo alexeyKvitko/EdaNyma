760406868e2b5be4a65952e7683a3eb65d935e18
akvitko <kpk_08@mail.ru>
2019-02-20 17:09:51 +0300

    Add code cdd dd

--- app/src/main/java/com/edanyma/activity/MainActivity.java
+++ app/src/main/java/com/edanyma/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 import com.edanyma.EdaNymaApp;
 import com.edanyma.R;
@@ -12,7 +13,7 @@ import com.edanyma.owncomponent.MainCardItem;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private static final int[] MAIN_CARDS = { R.id.wokCardId, R.id.burgerCardId, R.id.sushiCardId, R.id.pizzaCardId, R.id.restCardId };
+    private static final int[] MAIN_CARDS = { R.id.topLayoutId, R.id.middleLayoutId, R.id.bottomLayoutId };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +24,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        MainCardItem mainCardItem = null;
-//        Animation[] cardAnimation = new Animation[5];
-//        for( int i = 0; i < 5; i++ ){
-//            cardAnimation[i] = AnimationUtils.loadAnimation( EdaNymaApp.getAppContext(),R.anim.bounce);
-//            cardAnimation[i].setStartOffset( i*100 );
-//        }
-//        int idx = 0;
-//        for( int cardId : MAIN_CARDS ){
-//            mainCardItem = ( MainCardItem ) this.findViewById( cardId );
-//            mainCardItem.startAnimation( cardAnimation[idx] );
-//            idx++;
-//        }
+        LinearLayout mainCardItem = null;
+        Animation[] cardAnimation = new Animation[3];
+        for( int i = 0; i < 3; i++ ){
+            cardAnimation[i] = AnimationUtils.loadAnimation( EdaNymaApp.getAppContext(),R.anim.bounce);
+            cardAnimation[i].setStartOffset( i*100 );
+        }
+        int idx = 0;
+        for( int cardId : MAIN_CARDS ){
+            mainCardItem = ( LinearLayout ) this.findViewById( cardId );
+            mainCardItem.startAnimation( cardAnimation[idx] );
+            idx++;
+        }
     }
 
 
